7b7ca70fb2292a9d223b47b8c08f817fb323a5ad
shastel <max@shas.tel>
2018-11-05 12:59:29 +0300

    Fix issue with stdio

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cp = require('child_process');
 const cmd = process.argv.splice(2).join(' ');
 
 try {
-  cp.execSync(`git ${ru.toEn(cmd)}`)
+  cp.execSync(`git ${ru.toEn(cmd)}`, { stdio: 'inherit' });
 } catch (e) {
 
 }
